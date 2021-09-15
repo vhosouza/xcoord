FIX: Off-by-one error in computing mri2inv matrix
- The explanation: if the size of the image in y-dimension is, say,
  256, the image coordinates range from 0 to 255. The InVesalius space
  differs from the voxel space (for which the 'affine' matrix is defined)
  by the y-axis being inverted, that is, y-coordinate 0 (in InVesalius space)
  corresponds to 255 (in voxel space) and 255 corresponds to 0. Overall, that
  transformation can be done by subtracting the y-coordinate from zero
  (which is done elsewhere in the code) and translating it by 255, that is,
  translating by the image size minus one, not the image size. Hence the
  off-by-one error.

image_funcs.py
@@ -46,7 +46,7 @@ def mri2inv(imagedata, affine=None):
 
     # convert from invesalius 3D to RAS+ space
     mri2inv_mat = np.linalg.inv(affine.copy())
-    mri2inv_mat[1, 3] -= pix_dim[1] * img_shape[1]
+    mri2inv_mat[1, 3] -= pix_dim[1] * (img_shape[1] - 1)
 
     return mri2inv_mat
 
@@ -119,7 +119,7 @@ def inv2mri(imagedata, affine=None):
         affine = tf.compose_matrix(scale=None, shear=shear, angles=angs, translate=trans, perspective=persp)
 
     mri2inv = np.linalg.inv(affine.copy())
-    mri2inv[1, -1] -= pix_dim[1] * img_shape[1]
+    mri2inv[1, -1] -= pix_dim[1] * (img_shape[1] - 1)
     inv2mri_mat = np.linalg.inv(mri2inv)
 
     return inv2mri_mat